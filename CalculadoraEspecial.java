ac4d69bc5c6c1245281a16415ca329f263a3b51b
daniel21connor <jconnorr1@miumg.edu.gt>
2022-03-31 01:06:37 +0200

    se comienza a pensar en un menu para que el usuario pueda escoger las operaciones que el desea

---
+++ CalculadoraEspecial.java
@@ -0,0 +1,31 @@
+
+package calculadoraespecial;
+import java.util.Scanner; // usamos esto para, poder usar el Scanner 
+
+public class CalculadoraEspecial {
+ static  Scanner scanner=new Scanner (System.in);  //se escribe fuera del void para que sea una vaiable global
+ 
+    public static void main(String[] args) {
+    
+      
+     int opcion=0; //aca se pone opcion fuera del do para que pueda ser escogida en el do como variable global
+  // usamos do para poder repetir el menu
+  do{
+         
+     
+    System.out.print("\n Bienvenidos a calculadora especial:");
+    System.out.print("\n 1. Raiz cuadrada");
+    System.out.print("\n 2. numero factorial");
+    System.out.print("\n 3. area de un triangulo");
+    System.out.print("\n 4. potencia");
+    System.out.print("\n5. salir ");
+     
+
+
+   
+    
+    
+
+    
+
+
